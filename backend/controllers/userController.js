830caf59489187694ede4dbd466bbe9a374c3696
alexquar <alexanderquarrie@gmail.com>
2024-04-05 14:12:41 -0400

    MVC for auth

---
+++ backend/controllers/userController.js
@@ -0,0 +1,16 @@
+//login
+const mongoose = require('mongoose')
+const User = require('../models/userModel')
+const loginUser = async (res,req) => {
+
+}
+
+
+//signup
+const signupUser = async ()=>{
+
+}
+
+
+
+module.exports = {loginUser, signupUser}
---
+++ backend/routes/user.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const {loginUser,signupUser} = require('../controllers/userController')
+const router = express.Router()
+
+
+router.post('/login', loginUser)
+
+
+
+router.post('/signup', signupUser)
+
+
+
+
+
+
+
+
+
+
+
+
+module.export = router
