ff3a2969510ec74119e6ca9df41419d9bac92a12
alexquar <alexanderquarrie@gmail.com>
2024-04-05 08:47:53 -0400

    frontend setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
 
+      <Navbar />
+
+
+      <Routes>
+
+      <Route path='/' element={<Home/>} />
+
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom"
+export default function Navbar() {
+  return (
+   <header>
+    <div className="container">
+    <Link to='/'> U Fitness </Link>
+    </div>
+   </header>
+  )
+}
