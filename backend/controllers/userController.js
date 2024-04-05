01058e02659fd09a1b4261ef8596a3bfcdfddc7f
alexquar <alexanderquarrie@gmail.com>
2024-04-05 15:27:57 -0400

    tokens

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,11 @@
-//login
-const mongoose = require('mongoose')
+
 const User = require('../models/userModel')
+const jwt = require('jsonwebtocken')
+
+const createToken = (_id) => {
+   return jwt.sign({_id}, process.env.SECRET, {expiresIn:'1d'})
+}
+
 const loginUser = async (req,res) => {
 res.json({mssg:'login'})
 }
@@ -11,7 +16,8 @@ const signupUser = async (req,res)=>{
     const {email,password} = req.body
     try{
          const user = await User.signup(email,password)
-         res.status(200).json({email, user})
+        const token = createToken(user._id)
+         res.status(200).json({email, token})
     } catch (err) {
         res.status(400).json({error : err.message})
     }
