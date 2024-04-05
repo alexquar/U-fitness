41805f6e2be84de311a0ae8a7f40c1070cb78b3f
alexquar <alexanderquarrie@gmail.com>
2024-04-04 23:29:43 -0400

    patch and delete

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -1,5 +1,5 @@
 const Workout = require('../models/workoutModel')
-
+const mongoose = require('mongoose')
 const getWorkouts = async (req, res) => {
     const workouts = await Workout.find({}).sort({createdAt: -1})
   
@@ -33,11 +33,37 @@ const createWorkout = async (req,res) => {
 }
 
 const deleteWorkout = async (req, res) => {
+  const { id } = req.params
 
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({error: 'No such workout'})
+  }
+
+  const workout = await Workout.findOneAndDelete({_id: id})
+
+  if(!workout) {
+    return res.status(400).json({error: 'No such workout'})
+  }
+
+  res.status(200).json(workout)
 }
 
 const updateWorkout = async (req, res) => {
+  const { id } = req.params
 
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({error: 'No such workout'})
+  }
+
+  const workout = await Workout.findOneAndUpdate({_id: id}, {
+    ...req.body
+  })
+
+  if (!workout) {
+    return res.status(400).json({error: 'No such workout'})
+  }
+
+  res.status(200).json(workout)
 }
 
 module.exports = {
