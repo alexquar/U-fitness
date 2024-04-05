236b8934a0876a043cd57b97728b56362a626202
alexquar <alexanderquarrie@gmail.com>
2024-04-05 15:09:54 -0400

    validation

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
-
+const validator = require('validator')
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
@@ -16,11 +16,20 @@ const userSchema = new Schema({
 })
 
 userSchema.statics.signup = async function(email, password) {
+    if(!email || !password){
+        throw Error('Fill all fields...')
+    }
+    if(!validator.isEmail(email)){
+        throw Error('Enter a valid email...')
+    }
+    if(!validator.isStrongPassword(password)){
+        throw Error('Enter a strong password...')
+    }
 
     const exists = await this.findOne({ email })
   
     if (exists) {
-      throw Error('Email already in use')
+      throw Error('Email already in use...')
     }
 
     const salt = await bcrypt.genSalt(10)
