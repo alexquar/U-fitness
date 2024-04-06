069cf25b0315382dc1944093f793f595d03ae69d
alexquar <alexanderquarrie@gmail.com>
2024-04-06 10:06:51 -0400

    guarding frontend routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,10 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
+import { useAuthContext } from './hooks/useAuthContext';
+import { Navigate } from 'react-router-dom';
 function App() {
+  const {user} = useAuthContext()
   return (
     <div className="App">
       <BrowserRouter>
@@ -13,9 +16,9 @@ function App() {
     <div className="pages">
       <Routes>
 
-      <Route path='/' element={<Home/>} />
-      <Route path='/signup' element={<Signup />} />
-      <Route path='/login' element={<Login />} />
+      <Route path='/'  element={user ? <Home /> : <Navigate to="/login" />}  />
+      <Route path='/signup' element={ !user ? <Signup />: <Navigate to='/'/>} />
+      <Route path='/login' element={ !user ? <Login />: <Navigate to='/'/>} />
       </Routes>
       </div>
       </BrowserRouter>
