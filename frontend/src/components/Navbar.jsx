e7db38ded6716e0dddcfe68221e0427f515607cf
alexquar <alexanderquarrie@gmail.com>
2024-04-05 22:22:13 -0400

    conditional nav

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,17 +1,25 @@
 import { Link } from "react-router-dom"
+import { useLogout } from "../hooks/useLogout"
+import { useAuthContext } from "../hooks/useAuthContext"
 export default function Navbar() {
+  const {user} = useAuthContext()
+  const {logout}=useLogout()
   return (
    <header>
     <div className="container">
     <Link to='/'> <h1> U Fitness </h1> </Link>
     <nav>
+  {!user &&
     <div>
     <Link to='/login'> login </Link>
     <Link to='/signup'>  signup </Link>
     </div>
+  }
+  { user &&
     <div>
-<button className="btn"> Logout </button>
+<button className="btn" onClick={logout}> Logout </button>
     </div>
+  }
     </nav>
     </div>
    </header>
